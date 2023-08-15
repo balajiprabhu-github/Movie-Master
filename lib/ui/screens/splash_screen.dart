f90155787a7d5b323de2f86a70e5f51a46371f98
rbalajiprabhu <balajiprabhu008@gmail.com>
2023-08-15 13:12:17 +0530

    splash screen setup

---
+++ lib/ui/screens/splash_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:movies/ui/screens/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    _animation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    );
+
+    _animationController.forward();
+
+    _animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        // Animation completed, navigate to the main screen
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const HomeScreen()),
+        );
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ScaleTransition(
+          scale: _animation,
+          child: const ImageIcon(
+            AssetImage("assets/app-logo.png"),
+            color: Color(0xFFCD0404),
+            size: 150,
+          ),
+        ),
+      ),
+    );
+  }
+}
