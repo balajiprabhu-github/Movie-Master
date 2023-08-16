1b0d04502158df3cdba7eddedfe1be011092fd6b
rbalajiprabhu <balajiprabhu008@gmail.com>
2023-08-16 14:28:54 +0530

    home screen with movies list

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,14 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:movies/ui/data/api_service.dart';
+import 'package:movies/ui/data/model/trending_movies.dart';
+import 'package:movies/ui/widgets/movie_card_list.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late Future<List<Results>> trendingMovies;
+
+  @override
+  void initState() {
+    super.initState();
+    trendingMovies = fetchTrendingMovies();
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Movie App')),
-      body: const Center(
-        child: Text('Welcome to the Movie App!'),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Trending Movies',
+                style: TextStyle(
+                  fontSize: 20
+                ),
+              ),
+              const SizedBox(height: 12),
+              SizedBox(
+                child: FutureBuilder(
+                  future: trendingMovies,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) {
+                      return Center(
+                        child: Text(snapshot.error.toString()),
+                      );
+                    } else if (snapshot.hasData) {
+                      return  MovieCardList(snapshot: snapshot,);
+                    } else {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
